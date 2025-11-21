2d1404e995396989ee30d78625a93f8769c59b9c
Enstso <enstso@outlook.fr>
2025-11-21 23:02:02 +0100

    add the notification in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import {Component} from '@angular/core';
-import {
-  RouterOutlet
-} from '@angular/router';
+import {RouterOutlet} from '@angular/router';
 import {HeaderComponent} from './shared/components/header/header.component';
+import {Observable} from 'rxjs';
+import {NotificationService, Notification} from './core/services/notification/notification.service';
+import {AsyncPipe, NgClass, NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgClass, NgIf, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'event-planner';
+
+  readonly notification$: Observable<Notification | null>;
+
+  constructor(private readonly notificationService: NotificationService) {
+    this.notification$ = this.notificationService.notification$;
+  }
+
 }
--- src/app/features/auth/auth.module.ts
+++ src/app/features/auth/auth.module.ts
@@ -1,12 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {ReactiveFormsModule} from '@angular/forms';
+import {AuthRoutingModule} from './auth-routing.module';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule, LoginComponent, RegisterComponent]
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    AuthRoutingModule,
+    LoginComponent,
+    RegisterComponent
+  ]
 })
 export class AuthModule {}
